afcecf64ae740441513934d4e164ffbf3e077285
Taehyun Kim <persona7548@gmail.com>
2020-02-11 16:35:04 +0900

    Update exchangeID.py

--- exchangeID.py
+++ exchangeID.py
@@ -1,11 +1,19 @@
 import requests
 import time
 import pandas
+import json
 from bs4 import BeautifulSoup
 
-for i in range(0,9954):
-  csvfile = pandas.read_csv('C:/userID.csv',header=None,encoding='ANSI')
-  userID = csvfile[i][0]
-  print(userID)
-  f.close()
+f = open('C:/Users/KTH/Desktop/전공/python/userID.csv',encoding='ANSI')
+csvfile = pandas.read_csv('C:/Users/KTH/Desktop/전공/python/userID.csv',header=None,encoding='ANSI')
+f.close()
 
+f = open('C:/Users/KTH/Desktop/GitHub/realUserID.csv', 'a')
+for i in range(0,9948):
+    userID = str(csvfile[i][0])
+    url = 'https://api.neople.co.kr/cy/players?nickname='+userID+'&wordType=<wordType>&apikey=<secret>'
+    r = requests.get(url)
+    data = json.loads(r.text)
+    f.write(data["rows"][0]["playerId"]+',')
+    time.sleep(0.5)
+f.close()
