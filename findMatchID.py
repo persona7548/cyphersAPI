6d0206047c831d1b5867022d0d106c96651fd09a
Taehyun Kim <persona7548@gmail.com>
2020-02-14 14:33:55 +0900

    Update findMatchID.py

--- findMatchID.py
+++ findMatchID.py
@@ -2,19 +2,17 @@ import requests
 import time
 import pandas
 import json
-from bs4 import BeautifulSoup
 
-
-csvfile = pandas.read_csv('C:/Users/KTH/Desktop/python/realUserID.csv',header=None,encoding='ANSI')
-for j in range(0,9947):
-    time.sleep(1)
-    userID = str(csvfile[1][j])
-    if(userID != "nan"):
+csvfile = pandas.read_csv('C:/Users/KTH/Desktop/GitHub/matchId.csv',header=None,encoding='ANSI')
+for j in range(0,10000):
+    time.sleep(2)
+    try:
+        userID = str(csvfile[1][j])
         headers = {'Content-Type': 'application/json; charset=utf-8'}
-        url = 'https://api.neople.co.kr/cy/players/b7750d1ee3b5d9b24e520543b561f388/matches?gameTypeId=rating&startDate=20200101T0000&endDate=20200212T0000&limit=100&apikey=<SECRET>'
+        url = 'https://api.neople.co.kr/cy/players/'+userID+'/matches?gameTypeId=rating&limit=100&apikey='+API_KEY
         r = requests.get(url=url,headers = headers)
         data = json.loads(r.text)
-        for i in range(0,99):
+        for i in range(0,100):
             try:
                 f = open('C:/Users/KTH/Desktop/GitHub/matchId.csv', 'a')
                 matchId = str(data["matches"]["rows"][i]["matchId"])
@@ -22,4 +20,6 @@ for j in range(0,9947):
                 f.close()
             except:
                 break
+    except:
+        break
     print(j)
