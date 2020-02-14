2c68afb7b76d2956dc0adc5475acd869bc83fe92
Taehyun Kim <persona7548@gmail.com>
2020-02-14 14:48:47 +0900

    Update cypherCrawl.py

--- cypherCrawl.py
+++ cypherCrawl.py
@@ -1,8 +1,10 @@
 import requests
 import json
 
+API_KEY ='SECRET'
+
 for j in range(0,10000,1000):
-    url = 'https://api.neople.co.kr/cy/ranking/ratingpoint?&offset='+j+'&limit=1000&apikey='+API_KEY
+    url = 'https://api.neople.co.kr/cy/ranking/ratingpoint?&offset='+str(j)+'&limit=1000&apikey='+API_KEY
     r = requests.get(url)
     data = json.loads(r.text)
     for i in range(0,1000):
