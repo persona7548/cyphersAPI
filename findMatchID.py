e3a001a545ea284e620b3f5c9fcaeff8f6ae51bd
Taehyun Kim <persona7548@gmail.com>
2020-02-16 15:55:31 +0900

    Update findMatchID.py

--- findMatchID.py
+++ findMatchID.py
@@ -3,24 +3,27 @@ import time
 import pandas
 import json
 
+API_KEY = <SECRET>
+
+headers = {'Content-Type': 'application/json; charset=utf-8'}
 csvfile = pandas.read_csv('C:/Users/KTH/Desktop/GitHub/userID.csv',header=None,encoding='ANSI')
 csvfile = csvfile.drop_duplicates()
+
 for j in range(len(csvfile)):
-    time.sleep(2)
     try:
         userID = str(csvfile[1][j])
-        headers = {'Content-Type': 'application/json; charset=utf-8'}
-        url = 'https://api.neople.co.kr/cy/players/'+userID+'/matches?gameTypeId=rating&limit=100&apikey='+API_KEY
-        r = requests.get(url=url,headers = headers)
-        data = json.loads(r.text)
-        for i in range(0,100):
-            try:
-                f = open('C:/Users/KTH/Desktop/GitHub/matchId.csv', 'a')
-                matchId = str(data["matches"]["rows"][i]["matchId"])
-                f.write(matchId+'\n')
-                f.close()
-            except:
-                break
     except:
-        break
+        continue
+    url = 'https://api.neople.co.kr/cy/players/'+userID+'/matches?gameTypeId=rating&limit=100&apikey='+API_KEY
+    r = requests.get(url=url,headers = headers)
+    data = json.loads(r.text)
+    for i in range(0,100):
+        try:
+            f = open('C:/Users/KTH/Desktop/GitHub/matchId.csv', 'a')
+            matchId = str(data["matches"]["rows"][i]["matchId"])
+            f.write(matchId+'\n')
+            f.close()
+        except:
+            break
+    time.sleep(2)
     print(j)
