88760bbf2f4e26b817010ef70e002e8079b6c4f1
Taehyun Kim <persona7548@gmail.com>
2020-02-20 15:14:18 +0900

    Update findMatchID.py

--- findMatchID.py
+++ findMatchID.py
@@ -5,7 +5,7 @@ import json
 
 headers = {'Content-Type': 'application/json; charset=utf-8','apikey' :'SECRET'}
 csvfile = pandas.read_csv('C:/Users/KTH/Desktop/GitHub/userID.csv',header=None,encoding='ANSI')
-csvfile = csvfile.drop_duplicates()
+matchRecord = pandas.read_csv('C:/Users/KTH/Desktop/GitHub/matchId.csv')
 
 for j in range(len(csvfile)):
     try:
@@ -14,18 +14,28 @@ for j in range(len(csvfile)):
         continue
     next =''
     while next != "None":
+        time.sleep(0.1)
         url = 'https://api.neople.co.kr/cy/players/'+userID+'/matches?limit=100&next='+next
-        r = requests.get(url=url,headers = headers)
+        try:
+            r = requests.get(url=url,headers = headers)
+        except:
+            time.sleep(1)
+            print(id," 정지됨")
+            continue
         data = json.loads(r.text)
-        print(data)
-        next = str(data["matches"]["next"])
+        try:
+            next = str(data["matches"]["next"])
+        except:
+            break
         for i in range(0,100):
             try:
                 f = open('C:/Users/KTH/Desktop/GitHub/matchId.csv', 'a')
                 matchId = str(data["matches"]["rows"][i]["matchId"])
+                if matchId in matchRecord:
+                    continue
                 f.write(matchId+'\n')
                 f.close()
             except:
                 break
-    time.sleep(2)
+    time.sleep(0.1)
     print(j)
