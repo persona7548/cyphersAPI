92ba9f1672b3d075466fad3631ee56d0efeef677
Taehyun Kim <persona7548@gmail.com>
2020-02-23 03:28:53 +0900

    Update matchData.r

--- matchData.r
+++ matchData.r
@@ -120,38 +120,50 @@ for (k in list("탱커","서포터","근거리딜러","원거리딜러")){
   positionWin <- winMatch[winMatch$Position == k,]
   for (i in 1:nrow(CharRate)){
     charTmp<-positionInfo[positionInfo$Character.Id ==CharRate[[1]][i],]
-    #charWin <- positionWin[positionWin$Character.Id ==CharRate[[1]][5],]
-    if (nrow(charTmp)<(nrow(matchCount)*0.05)) next
-    nrow(charTmp)
-    tmp<-charTmp[7:36] 
+    charWin <- positionWin[positionWin$Character.Id ==CharRate[[1]][i],]
+    if (nrow(charTmp)<(nrow(matchCount)*0.05)) next ##픽률 5%미만은 pass
     itemlist<- data.frame(c(1:5))
+    tmp<-charTmp[7:36] 
     tmp<-table(tmp[12:14])
     tmp <- data.frame(tmp)
-    tmp <- tmp[tmp$Freq>0,]
-    tmp$Freq <- tmp$Freq/sum(tmp$Freq)
-    tmp <- tmp[order(-tmp[4]),]
+    winTmp <- charWin[7:36]
+    winTmp<-table(winTmp[12:14])
+    winTmp<- data.frame(winTmp)
+    tmp <-cbind(tmp,winTmp)
+    tmp <-cbind(tmp[1:4],tmp[8])
+    tmp[5] <- tmp[5]/tmp[4]
+    tmp[4] <- tmp[4]/sum(tmp[4])
     names(tmp)[4] <-c("픽률")
+    names(tmp)[5] <-c("승률")
+    tmp <- tmp[tmp[4]>0,]
+    tmp <- tmp[order(-tmp[4]),]
     tmp <-head(tmp,n=5)
     if(nrow(tmp)<5)
     {
-      for(j in nrow(tmp)+1:(5-nrow(tmp)))
-        tmp <- rbind(tmp,c("NA","NA","NA","NA"))
+      for(l in nrow(tmp)+1:(5-nrow(tmp)))
+        tmp <- rbind(tmp,c("NA","NA","NA","NA","NA"))
     }
     itemlist <- cbind(itemlist,tmp)
     for (j in 21:36){ #장비
       tmp <- table(charTmp[j])
       tmp <- data.frame(tmp)
+      winTmp<-table(charWin[j])
+      winTmp<- data.frame(winTmp)
+      tmp <-cbind(tmp,winTmp)
+      tmp <-cbind(tmp[1:2],tmp[4])
+      tmp[3] <- tmp[3]/tmp[2]
+      tmp[2]<- tmp[2]/sum(tmp[2])
       tmp <- tmp[tmp$Freq>0,]
-      tmp[2]<- tmp$Freq/sum(tmp$Freq)
-      tmp <- tmp[order(-tmp$Freq),]
+      tmp <- tmp[order(-tmp[2]),]
       tmp <-head(tmp,n=5)
       if(nrow(tmp)<5)
       {
-        for(j in nrow(tmp)+1:(5-nrow(tmp)))
-          tmp <- rbind(tmp,c("NA","NA"))
+        for(l in nrow(tmp)+1:(5-nrow(tmp)))
+          tmp <- rbind(tmp,c("NA","NA","NA"))
       }
       names(tmp)[1] <-c(colnames(charTmp[j]))
       names(tmp)[2] <-c("픽률")
+      names(tmp)[3] <-c("승률")
       itemlist <- cbind(itemlist,tmp)
     }
     Save <- paste0(CharRate[[1]][i],"_",k,".csv")
