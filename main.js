5400cf8cadf69ab04409cdb808669db07ea72ccb
Taehyun Kim <persona7548@gmail.com>
2020-03-06 22:52:37 +0900

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,7 @@ var ejs  = require('ejs');
 var mysql = require('mysql');
 var fs = require('fs');
 var path = require('path');
+
 var sanitizeHtml = require('sanitize-html');
 var qs = require('querystring');
 var template = require('./lib/template.js');
@@ -13,153 +14,33 @@ var template = require('./lib/template.js');
 var dbconfig = require('./config/database.js');
 var db = mysql.createConnection(dbconfig);
 db.connect();
+app.use(express.static('character'));
+
+
 app.get('/', function (request, response) {
-  fs.readdir('./character', function (error, filelist) {
+  fs.readdir('./character/thumb', function (error, filelist) {
     var title = 'Welcome';
     var description = 'Hello, Node.js';
     var list = template.list(filelist);
-    var html = template.HTML(title, list, `<h2>${title}</h2>${description}`,
-      `<a href="/create">create</a>
-     <a href="/persons">persons</a>
-     `);
+    var html = template.HTML(title, list, 
+      `<h2>${title}</h2>${description}`,
+      `<img src="https://img-api.neople.co.kr/cy/characters/d69971a6762d94340bb2332e8735238a" alt="${list}"/>
+      `);
     response.send(html);
   });
 });
-
-app.get('/index', function (request, response) {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
-  fs.readFile(__dirname + '/index.html', (err, data) => {
-    if (err) {
-      return console.error(err);
-    }
-    response.end(data, 'utf-8');
-  });
-});
-
 
 app.get('/character/:characterId', function (request, response) {
-  fs.readdir('./character', function (error, filelist) {
-    var filteredId = path.parse(request.params.characterId).base;
     fs.readFile(`list.html`, 'utf8', function (err, description) {
-      var title = request.params.characterId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(filelist);
-      db.query("SELECT * FROM `build` WHERE `character` LIKE \'" + title + "\' LIMIT 0, 30 ", function (err, rows) {
+      var title = path.basename(request.params.characterId,path.extname(request.params.characterId));
+      var cleanTitle = sanitizeHtml(title);
+      var html = (`<img src="/thumb/${title}.jpg" alt="${title}"/>`);
+      db.query("SELECT * FROM `build` WHERE `character` LIKE \'" + cleanTitle + "\' LIMIT 0, 30 ", function (err, rows) {
         if (err) throw err;
         response.send(ejs.render(description,
-          {pageName:title,prodList:rows}));
+          {pageName:cleanTitle,prodList:rows,img:html}));
       });
     });
-  });
-});
-
-
-app.get('/page/:pageId', function (request, response) {
-  fs.readdir('./data', function (error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      var title = request.params.pageId;
-      var sanitizedTitle = sanitizeHtml(title);
-      var sanitizedDescription = sanitizeHtml(description, {
-        allowedTags: ['h1']
-      });
-      var list = template.list(filelist);
-      var html = template.HTML(sanitizedTitle, list,
-        `<h2>${sanitizedTitle}</h2>${sanitizedDescription}`,
-        ` <a href="/create">create</a>
-          <a href="/update/${sanitizedTitle}">update</a>
-          <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${sanitizedTitle}">
-            <input type="submit" value="delete">
-          </form>`
-      );
-      response.send(html);
-    });
-  });
-});
-
-app.get('/create', function (request, response) {
-  fs.readdir('./data', function (error, filelist) {
-    var title = 'WEB - create';
-    var list = template.list(filelist);
-    var html = template.HTML(title, list, `
-      <form action="/create_process" method="post">
-        <p><input type="text" name="title" placeholder="title"></p>
-        <p>
-          <textarea name="description" placeholder="description"></textarea>
-        </p>
-        <p>
-          <input type="submit">
-        </p>
-      </form>
-    `, '');
-    response.send(html);
-  });
-});
-
-app.post('/create_process', function (request, response) {
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var title = post.title;
-    var description = post.description;
-    fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-      response.writeHead(302, { Location: `/?id=${title}` });
-      response.end();
-    })
-  });
-
-});
-
-app.get('/update/:pageId', function (request, response) {
-  fs.readdir('./data', function (error, filelist) {
-    var filteredId = path.parse(request.params.pageId).base;
-    fs.readFile(`data/${filteredId}`, 'utf8', function (err, description) {
-      var title = request.params.pageId;
-      var list = template.list(filelist);
-      var html = template.HTML(title, list,
-        `
-        <form action="/update_process" method="post">
-          <input type="hidden" name="id" value="${title}">
-          <p><input type="text" name="title" placeholder="title" value="${title}"></p>
-          <p>
-            <textarea name="description" placeholder="description">${description}</textarea>
-          </p>
-          <p>
-            <input type="submit">
-          </p>
-        </form>
-        `,
-        `<a href="/create">create</a> <a href="/update?id=${title}">update</a>`
-      );
-      response.send(html);
-    });
-  });
-});
-
-app.post('/update_process', function (request, response) {
-  var body = '';
-  request.on('data', function (data) {
-    body = body + data;
-  });
-  request.on('end', function () {
-    var post = qs.parse(body);
-    var id = post.id;
-    var title = post.title;
-    var description = post.description;
-    fs.rename(`data/${id}`, `data/${title}`, function (error) {
-      fs.writeFile(`data/${title}`, description, 'utf8', function (err) {
-        response.writeHead(302, { Location: `/?id=${title}` });
-        response.end();
-      })
-    });
-  });
 });
 
 app.listen(port, function () {
