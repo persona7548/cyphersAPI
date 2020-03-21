4fc414cc483ef28af47bbd2c0d8cd26753127489
Taehyun Kim <persona7548@gmail.com>
2020-03-21 18:41:17 +0900

    export sql other js file
    ./lib/sql.js

--- main.js
+++ main.js
@@ -5,82 +5,39 @@ const url = require('url');
 var ejs = require('ejs');
 var fs = require('fs');
 var path = require('path');
-var sanitizeHtml = require('sanitize-html');
-var qs = require('querystring');
-var template = require('./lib/template.js');
 var helmet = require('helmet');
+var requestAPI = require('request');
+var cheerio = require("cheerio");
+var sql = require("./lib/sql.js");
+
 app.use(helmet());
 app.use('/public', express.static('public'));
+
 var mysql = require('mysql');
 var dbconfig = require('./config/database.js');
-
 var db = mysql.createConnection(dbconfig);
 db.connect();
 
 app.get('/', function (request, response) {
-    var charInfo = 'SELECT * FROM `characters`';
-    db.query(charInfo, function (err, results) {
-        fs.readFile(`index.html`, 'utf8', function (err, description) {
-            response.send(ejs.render(description, {
-               prodList: results
-            }));
-        });
-    });
-});
-app.get('/contact', function (request, response) {
-      fs.readFile(`contact.html`, 'utf8', function (err, description) {
-          response.send(description);
-      });
-  });
-app.get('/user', function (request, response) {
-    fs.readFile(`user.html`, 'utf8', function (err, description) {
-        response.send(ejs.render(description, {userName: request.query.userName}));
-    });
-});
+  sql.index(request, response);});
 
+app.get('/user', function (request, response) {
+  sql.user(request, response);});
 
 app.get('/statistic', function (request, response) {
-  fs.readFile(`statistic.html`, 'utf8', function (err, description) {
-    if (err)  next(err);
-    var buildSql = 'SELECT * FROM positionInfo';
-    db.query(buildSql, function (err, rows) {
-      response.send(ejs.render(description,
-        { Info: rows }));
-    });
-  });
-});
+  sql.statistic(request, response);});
 
 app.get('/character/:characterId', function (request, response) {
-  fs.readFile(`list.html`, 'utf8', function (err, description) {
-    if (err) next(err);
-    var title = path.basename(request.params.characterId, path.extname(request.params.characterId));
-    var cleanTitle = sanitizeHtml(title);
-    var charSqlTemp = 'SELECT * FROM positionInfo WHERE `character` = ? ;';
-    var charSql = mysql.format(charSqlTemp,cleanTitle);
-    var buildSqlTemp = 'SELECT * FROM builddetail WHERE `character` = ?';
-    var buildSql = mysql.format(buildSqlTemp,cleanTitle);
-    db.query(charSql+buildSql, function (err, rows) {
-        response.send(ejs.render(description,
-          { pageName: cleanTitle,charInfo : rows[0], build: rows[1]}));
-      });
-    });
-  });
-
+  sql.character(request, response);});
+  
 app.get('/character/:characterId/:position', function (request, response) {
-  fs.readFile(`list.html`, 'utf8', function (err, description) {
-    if (err) next(err);
-    var title = path.basename(request.params.characterId, path.extname(request.params.characterId));
-    var cleanTitle = sanitizeHtml(title);
-    var cleanPosition = sanitizeHtml(request.params.position);
-    var charSql = 'SELECT * FROM positionInfo WHERE `character` = ' + mysql.escape(cleanTitle) + ';';
-    var buildSql = 'SELECT * FROM builddetail WHERE `character` = '+mysql.escape(cleanTitle)+' AND position = '+mysql.escape(cleanPosition)+' ';
-      db.query(charSql+buildSql, function (err, rows) {
-      response.send(ejs.render(description,
-        { pageName: cleanTitle,charInfo : rows[0], build: rows[1]}));
-    });
+  sql.position(request, response);});
+
+app.get('/contact', function (request, response) {
+  fs.readFile(`contact.html`, 'utf8', function (err, description) {
+      response.send(description);
   });
 });
-
 
 app.use(function(req, res, next) {
   res.status(404).send('Sorry cant find that!');
@@ -91,8 +48,6 @@ app.use(function (err, req, res, next) {
   res.status(500).send('Something broke!')
 });
  
-
-
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 });
