e92d69d91862f04a94a3eaa546f3f7a38f0f82ce
KimTaeHyun <persona7548@gmail.com>
2023-06-09 00:39:47 +0900

    [oxhorn 2.0]

--- main.js
+++ main.js
@@ -9,8 +9,18 @@ var helmet = require('helmet');
 var requestAPI = require('request');
 var cheerio = require("cheerio");
 var sql = require("./lib/sql.js");
+const { expressCspHeader, INLINE, NONE, SELF } = require('express-csp-header');
 
+app.use(expressCspHeader({
+    directives: {
+        'script-src': [SELF, INLINE,,"cdn.jsdelivr.net/npm/apexcharts","https://cdn.datatables.net/v/bs4/jq-3.3.1/dt-1.10.20/sp-1.0.1/sl-1.3.1/datatables.min.js"
+        ,"https://cdn.jsdelivr.net/npm/chart.js", "https://www.google-analytics.com/analytics.js","https://cdnjs.cloudflare.com", "https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js",
+        "https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js", "https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js","https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js",
+        "https://unpkg.com","*.googletagmanager.com"],
+    }
+}));
 app.use(helmet());
+
 app.use('/public', express.static('public'));
 
 var mysql = require('mysql');
@@ -21,12 +31,22 @@ db.connect();
 app.get('/', function (request, response) {
   sql.index(request, response);});
 
-app.get('/user', function (request, response) {
-  sql.user(request, response);});
+  app.get('/robots.txt', function (request, response) {
+    fs.readFile(`robots.txt`, 'utf8', function (err, description) {
+      response.send(description);
+  });});
+
+app.get('/match', function (request, response) {
+    sql.match(request, response);});
 
 app.get('/ranking', function (request, response) {
   sql.ranking(request, response);});
-  
+
+app.get('/user', function (request, response) {
+  sql.user(request, response);});
+app.get('/vs', function (request, response) {
+  sql.vs(request, response);});
+
 app.get('/statistic', function (request, response) {
   sql.statistic(request, response);});
 
@@ -43,14 +63,20 @@ app.get('/contact', function (request, response) {
 });
 
 app.use(function(req, res, next) {
-  res.status(404).send('Sorry cant find that!');
+  fs.readFile(`contact.html`, 'utf8', function (err, description) {
+    res.status(404).send(description);
+});
 });
  
 app.use(function (err, req, res, next) {
   console.error(err.stack)
-  res.status(500).send('Something broke!')
+  fs.readFile(`contact.html`, 'utf8', function (err, description) {
+    res.status(500).send(description);
+});
 });
  
 app.listen(port, function () {
   console.log(`Example app listening on port ${port}!`)
 });
+
+
